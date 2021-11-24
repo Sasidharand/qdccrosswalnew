071b9a7f392e4a2c88ff983bdf50a0950b498828
Sasidharand <86399143+Sasidharand@users.noreply.github.com>
2021-11-24 22:51:37 +0530

    Update JDBCConnection.java

--- src/com/qdcdata/JDBCConnection.java
+++ src/com/qdcdata/JDBCConnection.java
@@ -26,17 +26,11 @@ public class JDBCConnection  {
 		
 		conn = null;
 		
-String dbName = "QDC";
 		
-		//String dbName=dbname;
-		String serverip="164.52.218.134";
-		String databaseUserName = "sa";
-		String databasePassword = "Pione@123";
-		
-	/*	String dbName = "QDC";
+		String dbName = "QDC";
 		String serverip="qoipr.database.windows.net";
 		String databaseUserName = "qoipradmin";
-		String databasePassword = "QoipR242002";*/
+		String databasePassword = "QoipR242002";
 		
 		
 		String serverport="1433";
